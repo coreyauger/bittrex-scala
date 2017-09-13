acb812d68412ff0e5a59b92581b3ed097b880b94
= <coreyauger@gmail.com>
2017-09-12 23:31:24 -0700

    reanme

--- src/main/scala/io/surfkit/derpyhoves/flows/Bittrex.scala
+++ src/main/scala/io/surfkit/derpyhoves/flows/Bittrex.scala
@@ -21,96 +21,10 @@ import org.joda.time.DateTime
 /**
   * Created by suroot on 18/07/17.
   */
-object AlphaVantage{
-  sealed trait AV
+object Bitrex{
+  sealed trait BX
 
   val API_KEY = "XXX"
-
-  case class Interval(period: String) extends AV{
-    def toDuration = this match{
-      case AlphaVantage.Interval.`1min` => 1 minute
-      case AlphaVantage.Interval.`5min` => 5 minutes
-      case AlphaVantage.Interval.`15min` => 15 minutes
-      case AlphaVantage.Interval.`30min` => 30 minutes
-      case AlphaVantage.Interval.`60min` => 60 minutes
-      case AlphaVantage.Interval.daily => 1 day
-      case AlphaVantage.Interval.weekly => 7 days
-      case AlphaVantage.Interval.monthly => 30 days
-      case _ => 24 hours
-    }
-  }
-  object Interval{
-    val `1min` = Interval("1min")
-    val `5min` = Interval("5min")
-    val `15min` = Interval("15min")
-    val `30min` = Interval("30min")
-    val `60min` = Interval("60min")
-    val daily = Interval("daily")
-    val weekly = Interval("weekly")
-    val monthly = Interval("monthly")
-  }
-
-  final case class IntervalPrice(
-                `1. open`: String,
-                `2. high`: String,
-                `3. low`: String,
-                `4. close`: String,
-                `5. volume`: String) extends AV
-  implicit val tickWrites = Json.writes[IntervalPrice]
-  implicit val tickReads = Json.reads[IntervalPrice]
-
-  final case class TimeSeries(series: Seq[(String, IntervalPrice)]) extends AV
-  implicit val tsFormat: Format[TimeSeries] =
-    new Format[TimeSeries] {
-      override def reads(json: JsValue): JsResult[TimeSeries] = json match {
-        case j: JsObject =>
-          JsSuccess(TimeSeries(j.fields.map {
-            case (name, size) =>
-              size.validate[IntervalPrice] match {
-                case JsSuccess(validSize, _) => (name, validSize)
-                case e: JsError => return e
-              }
-          }))
-        case _ =>
-          JsError("Invalid JSON type")
-      }
-
-      override def writes(o: TimeSeries): JsValue = Json.toJson(o.series.toMap)
-    }
-
-  case class MetaData(
-                 `1. Information`: String,
-                 `2. Symbol`: String,
-                 `3. Last Refreshed`: String,
-                 `4. Interval`: String,
-                 `5. Output Size`: String,
-                 `6. Time Zone`: String
-   ) extends AV
-  implicit val mdWrites = Json.writes[MetaData]
-  implicit val mdReads = Json.reads[MetaData]
-
-  case class MetaDataDaily(
-                       `1. Information`: String,
-                       `2. Symbol`: String,
-                       `3. Last Refreshed`: String,
-                       `4. Output Size`: String,
-                       `5. Time Zone`: String
-                     ) extends AV
-  implicit val mddWrites = Json.writes[MetaDataDaily]
-  implicit val mddReads = Json.reads[MetaDataDaily]
-
-  case class MetaDataEMA(
-                          `1: Symbol`: String,
-                          `2: Indicator`: String,
-                          `3: Last Refreshed`: String,
-                          `4: Interval`: String,
-                          `5: Time Period`: Int,
-                          `6: Series Type`: String,
-                          `7: Time Zone`: String
-                          ) extends AV
-  implicit val mddEMAWrites = Json.writes[MetaDataEMA]
-  implicit val mddEMAReads = Json.reads[MetaDataEMA]
-
 
   case class Market(
                      MarketCurrency: String,
@@ -124,12 +38,12 @@ object AlphaVantage{
                      Notice: Option[String],
                      IsSponsored: Option[Boolean],
                      LogoUrl: Option[String]
-                   ) extends AV
+                   ) extends BX
   implicit val marketWrites = Json.writes[Market]
   implicit val marketReads = Json.reads[Market]
 
 
-  case class Response[T <: AV](success: Boolean, message: String, result: Seq[T]) extends AV
+  case class Response[T <: BX](success: Boolean, message: String, result: Seq[T]) extends BX
   implicit val responseMarketWrites = Json.writes[Response[Market]]
   implicit val responseMarketReads = Json.reads[Response[Market]]
 
@@ -159,9 +73,9 @@ object AlphaVantage{
 
 }
 
-class BittrexInterval[T <: AlphaVantage.AV](function: String,symbol: String, interval: AlphaVantage.Interval, tz: DateTimeZone)(implicit system: ActorSystem, materializer: Materializer, um: Reads[T]) extends BittrexPoller(
-  url = s"https://www.alphavantage.co/query?function=${function}&symbol=${symbol}&interval=${interval.period}&apikey=${AlphaVantage.API_KEY}",
-  interval = interval.toDuration, Some(tz)) with PlayJsonSupport{
+class BittrexInterval[T <: Bitrex.BX(function: String,symbol: String, interval: FiniteDuration, tz: DateTimeZone)(implicit system: ActorSystem, materializer: Materializer, um: Reads[T]) extends BittrexPoller(
+  url = s"https://www.alphavantage.co/query?function=${function}&symbol=${symbol}&interval=${interval}&apikey=${Bitrex.API_KEY}",
+  interval = interval, Some(tz)) with PlayJsonSupport{
 
   def json(): Source[Try[Future[T]], Cancellable] = super.apply().map{
     case scala.util.Success(response) => scala.util.Success(Unmarshal(response.entity).to[T])
@@ -176,18 +90,18 @@ class BittrexInterval[T <: AlphaVantage.AV](function: String,symbol: String, int
 
 class BittrexApi(implicit system: ActorSystem, materializer: Materializer) extends PlayJsonSupport {
 
-  val aoiKey = "XXX"
+  val aoiKey = "390521dbfd4040c28e6f536e209cb94"
 
   def nounce = new DateTime().getMillis() / 1000
   object api extends BittrexSignedRequester
 
-  def getMarkets()(implicit um: Reads[AlphaVantage.Response[AlphaVantage.Market]]) = {
+  def getMarkets()(implicit um: Reads[Bitrex.Response[Bitrex.Market]]) = {
     api.get(s"https://bittrex.com/api/v1.1/public/getmarkets?apikey=${aoiKey}&nonce=${nounce}").flatMap { response =>
       /*val bs: Future[ByteString] = response.entity.toStrict(1 minute).map { _.data }
       val s: Future[String] = bs.map(_.utf8String) // if you indeed need a `String`
       s.foreach(println)
 */
-      Unmarshal(response.entity).to[AlphaVantage.Response[AlphaVantage.Market]]
+      Unmarshal(response.entity).to[Bitrex.Response[Bitrex.Market]]
     }
   }
 }
