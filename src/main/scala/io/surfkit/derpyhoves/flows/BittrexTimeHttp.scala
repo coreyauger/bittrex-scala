ec960af7800ef6f7f3cea4ede6bc2f5dd87653d8
= <coreyauger@gmail.com>
2017-09-12 23:25:38 -0700

    refactor

--- src/main/scala/io/surfkit/derpyhoves/flows/BittrexTimeHttp.scala
+++ src/main/scala/io/surfkit/derpyhoves/flows/BittrexTimeHttp.scala
@@ -4,21 +4,28 @@ import akka.NotUsed
 import akka.actor.{ActorSystem, Cancellable}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse, Uri}
 import akka.stream.Materializer
 import akka.stream.scaladsl.Source
 import org.joda.time.DateTimeZone
 import org.joda.time.DateTime
+import com.roundeights.hasher.Implicits._
 
 import scala.concurrent.duration.FiniteDuration
 import scala.util.Try
 
+trait Bittrex{
+  val apisecret = "602ce0c2b016466ab8b5b8d63c77d4b7"
+}
+
 /**
   * Created by suroot on 18/07/17.
   */
-class TimeSeries(url: String, interval: FiniteDuration, hoursOpt: Option[DateTimeZone] = None)(implicit system: ActorSystem, materializer: Materializer) {
+class BittrexPoller(url: String, interval: FiniteDuration, hoursOpt: Option[DateTimeZone] = None)(implicit system: ActorSystem, materializer: Materializer) extends Bittrex {
   import scala.concurrent.duration._
-  val request: _root_.akka.http.scaladsl.model.HttpRequest = RequestBuilding.Get(Uri(url))
+  val apisign = url.hmac(apisecret).sha512
+  val request: _root_.akka.http.scaladsl.model.HttpRequest = RequestBuilding.Get(Uri(url)).addHeader(RawHeader("apisign", apisign))
   val source: Source[HttpRequest, Cancellable] = Source.tick(0.seconds, interval, request).filter{ _ =>
     hoursOpt.map{ timezone =>
       val dt = new DateTime(timezone)
@@ -32,5 +39,11 @@ class TimeSeries(url: String, interval: FiniteDuration, hoursOpt: Option[DateTim
   def shutdown = {
     Http().shutdownAllConnectionPools()
   }
+}
 
+class BittrexSignedRequester(implicit system: ActorSystem, materializer: Materializer) extends Bittrex{
+  def get(url: String) = {
+    val apisign = url.hmac(apisecret).sha512
+    Http().singleRequest(HttpRequest(uri = url).addHeader(RawHeader("apisign", apisign)))
+  }
 }
